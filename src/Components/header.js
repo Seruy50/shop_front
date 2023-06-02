beab088c4426a944ed0d3b1f8567e618362a65e0
Seruy50 <Seruy50@ukr.net>
2023-06-02 17:14:48 +0300

    al

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { MainPage } from './MainPage.js';
-import { ShopingCart } from './ShopingCart';
-import axios from 'axios';
 
 export function Header() {
 
     return  <header>
                 <div className="header__links">
                     <div className="header__links__one">
-                        <p onClick={tryr}><Link to='/'>Shop</Link></p>
+                        <p><Link to='/'>Shop</Link></p>
                     </div>
                     <span className="header__links__span"></span>
                     <div className="header__links__one">
